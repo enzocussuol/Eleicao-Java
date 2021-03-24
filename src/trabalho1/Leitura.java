28419ca54d39e44472bbd8c652bc25d12e31ea88
enzo <enzo@192.168.86.39>
2021-03-23 22:04:05 -0300

    Leitura do arquivo de politicos finalizada.
    Retirei o construtor da classe Politico pq n ia precisar usar, criei um
    toString tb pra poder imprimir.
    
    Usei o localDate.

--- src/trabalho1/Leitura.java
+++ src/trabalho1/Leitura.java
@@ -2,6 +2,8 @@ package trabalho1;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -9,13 +11,44 @@ public class Leitura {
 	public void lePoliticos(LinkedList<Politico> politicos){
 		try {
 			FileInputStream input = new FileInputStream("src/csv-exemplos/capitais/vitória-candidatos.csv");
-			Scanner scanner = new Scanner(input, "UTF-8");
+			Scanner inputScanner = new Scanner(input, "UTF-8");
 			
-			System.out.println(scanner.nextLine());
+			String linha;
 			
-			scanner.close();
+			inputScanner.nextLine();
+			while(inputScanner.hasNextLine()) {
+				linha = inputScanner.nextLine();
+				
+				Scanner linhaScanner = new Scanner(linha);
+				linhaScanner.useDelimiter(",");
+				
+				Politico politico = new Politico();
+				
+				politico.setNumero(Integer.parseInt(linhaScanner.next()));
+				politico.setVotosNominais(Integer.parseInt(linhaScanner.next()));
+				politico.setSituacao(linhaScanner.next());
+				politico.setNome(linhaScanner.next());
+				politico.setNomeUrna(linhaScanner.next());
+				politico.setSexo(linhaScanner.next().charAt(0));
+				
+				DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+				LocalDate data = LocalDate.parse(linhaScanner.next(), formatador);
+				
+				politico.setDataNascimento(data);
+				
+				if(linhaScanner.next().equals("Válido")) {
+					politico.setPartido(Integer.parseInt(linhaScanner.next()));
+					politicos.add(politico);
+				}else {
+					linhaScanner.next();
+				}
+				
+				linhaScanner.close();
+			}
+			
+			inputScanner.close();
 		} catch(FileNotFoundException e){
-			System.out.println("Nao foi possivel ler o arquivo");
+			System.out.println("Nao foi possivel abrir o arquivo de leitura");
 			return;
 		}
 	}
--- src/trabalho1/Politico.java
+++ src/trabalho1/Politico.java
@@ -9,31 +9,8 @@ public class Politico {
 	private String nome;
 	private String nomeUrna;
 	private char sexo;
-
-	private LocalDate dataNascimento; //verificar como funciona date
-	private String destinoVoto;
+	private LocalDate dataNascimento;
 	private int partido;
-	
-	public Politico() {
-		
-	}
-	
-	public Politico(int numero, int votos_nominais, String situacao, String nome, String nome_urna, char sexo, LocalDate data_nascimento ,String destino_voto, int partido) {
-		this.setNumero(numero);
-		this.setVotosNominais(votos_nominais);
-		this.setSituacao(situacao);
-		this.setNome(nome);
-		this.setNomeUrna(nome_urna);
-		this.setSexo(sexo);
-		this.dataNascimento = data_nascimento;
-		this.setDestinoVoto(destino_voto);
-
-		this.setPartido(partido);
-	}
-	
-	public void apresentarPolitico() {
-		//imprimir politico
-	}
 
 	public int getNumero() {
 		return numero;
@@ -42,7 +19,6 @@ public class Politico {
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-
 
 	public int getVotosNominais() {
 		return votosNominais;
@@ -68,7 +44,6 @@ public class Politico {
 		this.nome = nome;
 	}
 
-
 	public String getNome_urna() {
 		return nomeUrna;
 	}
@@ -93,14 +68,6 @@ public class Politico {
 		this.dataNascimento = data_nascimento;
 	}
 
-	public String getDestinoVoto() {
-		return destinoVoto;
-	}
-
-	public void setDestinoVoto(String destino_voto) {
-		this.destinoVoto = destino_voto;
-	}
-
 	public int getPartido() {
 		return partido;
 	}
@@ -109,5 +76,15 @@ public class Politico {
 		this.partido = partido;
 	}
 	
-	
+	@Override
+	public String toString() {
+		return this.numero + "," + 
+				this.votosNominais + "," + 
+				this.situacao + "," + 
+				this.nome + "," + 
+				this.nomeUrna + "," + 
+				this.sexo + "," + 
+				this.dataNascimento + "," + 
+				this.partido;
+	}
 } 
