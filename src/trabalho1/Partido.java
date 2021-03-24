e7cead36e699141b9aba7241bca6baed7b24e232
Pichau <Pichau@DESKTOP-79V9UO5>
2021-03-24 15:57:05 -0300

    Partidos: adicao da lista de politicos do partido e sua quantidade

--- src/trabalho1/Partido.java
+++ src/trabalho1/Partido.java
@@ -1,10 +1,15 @@
 package trabalho1;
 
+import java.util.LinkedList;
+
 public class Partido {
 	private int numeroPartido;
 	private int votosLegenda;
 	private String nomePartido;
 	private String siglaPartido;
+	private LinkedList<Politico> politicos; 
+	private int quantidadePoliticos;
+	
 	
 	public int getNumeroPartido() {
 		return numeroPartido;
@@ -44,5 +49,30 @@ public class Partido {
 				this.votosLegenda + "," + 
 				this.nomePartido + "," + 
 				this.siglaPartido;
+	}
+
+	public int getQuantidadeCandidatos(LinkedList<Politico> politicos) {
+		return this.quantidadePoliticos;
+	}
+
+	public void setQuantidadePoliticos(int quantidadePoliticos) {
+		this.quantidadePoliticos = 0;
+		for(Politico politico : politicos) {
+			if(politico.getPartido() == this.numeroPartido) {
+				this.quantidadePoliticos++;				
+			}
+		}
+	}
+
+	public LinkedList<Politico> getPoliticos() {
+		return this.politicos;
+	}
+
+	public void setPoliticos(LinkedList<Politico> politicos) {
+		for(Politico politico : politicos) {
+			if(politico.getPartido() == this.numeroPartido) {
+				this.politicos.add(politico);
+			}
+		}
 	}
 }
